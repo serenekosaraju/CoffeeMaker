67e49eec26da5d9e756ae46bc977531ae4e3a51b
serenek <serene.kosaraju@gmail.com>
2021-04-10 11:16:37 +0530

    removed hardcoded ingredients corresponding to the example given

--- coffee_machine.py
+++ coffee_machine.py
@@ -9,13 +9,12 @@ class CoffeeMachine:
         self.cardamom_syrup = cardamom_syrup
         self.ginger_syrup = ginger_syrup
         self.sugar_syrup = sugar_syrup
-        self.cups = cups
         self.hot_water = water
         self.hot_milk = milk
         self.beverages = beverages
         self.not_available = ""
         self.choice = 0
-        self.reduced = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.reduced = [0, 0, 0, 0, 0, 0, 0]
         self.money = 0
         self.count_n = 1
         self.running = False
@@ -50,13 +49,11 @@ class CoffeeMachine:
             self.not_available = "ginger_syrup"
         elif self.cardamom_syrup - self.reduced[3] < 0:
             self.not_available = "cardamom_syrup"
-        elif self.cups - self.reduced[4] < 0:
-            self.not_available = "cups"
-        elif self.hot_water - self.reduced[5] < 0:
+        elif self.hot_water - self.reduced[4] < 0:
             self.not_available = "hot_water"
-        elif self.hot_milk - self.reduced[6] < 0:
+        elif self.hot_milk - self.reduced[5] < 0:
             self.not_available = "hot_milk"
-        elif self.sugar_syrup - self.reduced[7] < 0:
+        elif self.sugar_syrup - self.reduced[6] < 0:
             self.not_available = "sugar_syrup"
         if self.not_available != "":  # if something was detected to be below zero after deduction
             return False
@@ -64,48 +61,37 @@ class CoffeeMachine:
             return True
 
     def deduct_supplies(self):  # performs operation from the reduced list, based on the coffee chosen
-        self.hot_water -= self.reduced[5]
-        self.hot_milk -= self.reduced[6]
+        self.hot_water -= self.reduced[4]
+        self.hot_milk -= self.reduced[5]
         self.coffee_syrup -= self.reduced[0]
         self.cardamom_syrup -= self.reduced[3]
         self.tea_leaves_syrup -= self.reduced[1]
         self.ginger_syrup -= self.reduced[2]
-        self.sugar_syrup -= self.reduced[7]
-        self.cups -= self.reduced[4]
+        self.sugar_syrup -= self.reduced[6]
 
     def buy(self):
-        beverages_catered = {"hot_ginger_tea": [0, 10, 5, 0, 1, 50, 10, 10], "cardamom_tea": [0, 10, 0, 5, 1, 50, 10, 10],
-                             "hot_coffee": [10, 0, 0, 0, 1, 50, 10, 10], "hot_milk": [0, 0, 0, 0, 10, 0, 50, 0],
-                             "hot_water": [0, 0, 0, 0, 10, 50, 0, 0]}
+
         for beverage in self.beverages:
-            #Set of beverages tested against pre defined value arrays based on beverages catered by the coffee maker
-            if beverage in beverages_catered.keys():
-                self.reduced = beverages_catered[beverage]
+            ingredients = self.beverages[beverage].keys()
+            ingredients_available = ["coffee_syrup", "tea_leaves_syrup", "ginger_syrup", "cardamom_syrup",
+                                     "hot_water", "hot_milk", "sugar_syrup"]
+            if set(ingredients).issubset(ingredients_available):
+                for index, ingredient in enumerate(ingredients_available):
+                    if ingredient in ingredients:
+                        self.reduced[index] = self.beverages[beverage][ingredient]
+                    else:
+                        self.reduced[index] = 0
                 if self.available_check():  # checks if supplies are available
                     self.deduct_supplies()  # if it is, then it deducts
                     print("{} is prepared".format(beverage))
                 else:
                     print("{} cannot be prepared because {} is not available".format(beverage, self.not_available))
-            #Set of new beverages unknown to the coffee maker
+
+            # Ingredients not in available set of ingredients
             else:
-                ingredients_available = ["coffee_syrup", "tea_leaves_syrup", "ginger_syrup", "cardamom_syrup", "cups",
-                                         "hot_water", "hot_milk", "sugar_syrup"]
-                ingredients_required_for_beverage = self.beverages[beverage].keys()
-                if set(ingredients_required_for_beverage).issubset(ingredients_available):
-                    for index, ingredient in enumerate(ingredients_available):
-                        if ingredient in ingredients_required_for_beverage:
-                            self.reduced[index] = self.beverages[beverage][ingredient]
-                        else:
-                            self.reduced[index] = 0
-                    if self.available_check():  # checks if supplies are available
-                        self.deduct_supplies()  # if it is, then it deducts
-                        print("{} is prepared".format(beverage))
-                    else:
-                        print("{} cannot be prepared because {} is not available".format(beverage, self.not_available))
-                else:
-                    for ingredient in self.beverages[beverage].keys():
-                        if ingredient not in ingredients_available:
-                            print("{} can't be prepared because {} not available".format(beverage, ingredient))
+                for ingredient in self.beverages[beverage].keys():
+                    if ingredient not in ingredients_available:
+                        print("{} can't be prepared because {} not available".format(beverage, ingredient))
 
         self.start()
 
@@ -137,7 +123,6 @@ class CoffeeMachine:
         self.ginger_syrup += int(input("how many ml of ginger syrup do you want to add:\n"))
         self.tea_leaves_syrup += int(input("how many ml of tea leaves syrup do you want to add:\n"))
         self.sugar_syrup += int(input("how many ml of sugar syrup do you want to add:\n"))
-        self.cups += int(input("Write how many cups of coffee do you want to add:\n"))
         self.return_to_menu()
 
     def status(self):  # to display the quantities of supplies in the machine at the moment
@@ -149,14 +134,12 @@ class CoffeeMachine:
         print("{} of ginger_syrup".format(self.ginger_syrup))
         print("{} of sugar_syrup".format(self.sugar_syrup))
         print("{} of tea_leaves_syrup".format(self.tea_leaves_syrup))
-        print("{} of cups".format(self.cups))
         self.return_to_menu()
 
 
 if __name__ == "__main__":
     from test_input import ip
 
-    num_outlets_1 = int(ip["machine"]["outlets"]["count_n"])
     num_outlets = int(ip["machine"]["outlets"]["count_n"])
     total_items_quantity = ip["machine"]["total_items_quantity"]
     ginger_syrup = total_items_quantity['ginger_syrup']
@@ -171,4 +154,3 @@ if __name__ == "__main__":
     cf_init = CoffeeMachine(coffee_syrup, tea_leaves_syrup, ginger_syrup, cardamom_syrup, num_cups, hot_water, hot_milk,
                             sugar_syrup, beverages)
     cf_init.main_task()
-
